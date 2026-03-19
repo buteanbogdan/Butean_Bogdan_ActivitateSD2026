515afadd2c04e82588926f3997744b7435b3b6b4
buteanbogdan <b.bogdan2302@gmail.com>
2026-03-19 20:50:47 +0200

    add lista simpla

--- Laborator02.c
+++ Laborator02.c
@@ -1,137 +1,137 @@
-#include<stdio.h>
-#include<malloc.h>
-
-
-struct Laptop {
-	int id;
-	int RAM;
-	char* producator;
-	float pret;
-	char serie;
-};
-
-struct Laptop initializare(int id, int
-	ram, const char* producator, float pret, char serie) {
-	struct Laptop l;
-	l.id = id;
-	l.RAM = ram;
-	l.producator = (char*)malloc(sizeof(char) * (strlen(producator) + 1));
-	strcpy_s(l.producator, strlen(producator) + 1, producator);
-	l.pret = pret;
-	l.serie = serie;
-	return l;
-}
-
-void afisare(struct Laptop l) {
-	if (l.producator != NULL) {
-		printf("%d. Laptopul %s seria %c are %d GB RAM si costa %5.2f RON.\n", l.id, l.producator, l.serie, l.RAM, l.pret);
-	}
-	else {
-		printf("%d. Laptopul din seria %c are %d GB RAM si costa %5.2f RON.\n", l.id, l.serie, l.RAM, l.pret);
-	}
-}
-
-void afisareVector(struct Laptop* vector, int nrElemente) {
-	for (int i = 0; i < nrElemente; i++) {
-		afisare(vector[i]);
-	}
-}
-
-struct Laptop* copiazaPrimeleNElemente(struct Laptop* vector, int nrElemente, int nrElementeCopiate) {
-	struct Laptop* vectorNou = NULL;
-	vectorNou = (struct Laptop*)malloc(sizeof(struct Laptop) * nrElementeCopiate);
-	for (int i = 0; i < nrElementeCopiate; i++) {
-		vectorNou[i] = vector[i];
-		vectorNou[i].producator = (char*)malloc(strlen(vector[i].producator) + 1);
-		strcpy_s(vectorNou[i].producator, strlen(vector[i].producator) + 1, vector[i].producator);
-	}
-
-	return vectorNou;
-}
-
-void dezalocare(struct Laptop** vector, int* nrElemente) {
-	for (int i = 0; i < (*nrElemente); i++) {
-		if ((*vector)[i].producator != NULL){
-		free((*vector)[i].producator);
-		}
-	}
-	free(*vector);
-	*vector = NULL;
-	*nrElemente = 0;
-}
-
-void copiazaLaptopuriScumpe(struct Laptop* vector, char nrElemente, float pretMinim, struct Laptop** vectorNou, int* dimensiune) {
-	*dimensiune = 0;
-	for (int i = 0; i < nrElemente; i++) {
-		if (vector[i].pret >= pretMinim) {
-			(*dimensiune)++;
-		}
-	}
-	if ((*vectorNou) != NULL){
-		free(*vectorNou);
-	}
-	*vectorNou = (struct Laptop*)malloc(sizeof(struct Laptop) * (*dimensiune));
-	int k = 0;
-	for (int i = 0; i < nrElemente; i++) {
-		if (vector[i].pret >= pretMinim) {
-			(*vectorNou)[k] = vector[i];
-			(*vectorNou)[k].producator = (char*)malloc(strlen(vector[i].producator) + 1);
-			strcpy_s((*vectorNou)[k].producator, strlen(vector[i].producator) + 1, vector[i].producator);
-			k++;
-		}
-	}
-}
-
-struct Laptop getPrimulLaptopByProducator(struct Laptop* vector, int nrElemente, const char* producator) {
-	struct Laptop l;
-	for (int i = 0; i < nrElemente; i++) {
-		if (strcmp(vector[i].producator, producator) == 0) {
-			l = vector[i];
-			l.producator = (char*)malloc(strlen(vector[i].producator) + 1);
-			strcpy_s(l.producator, strlen(vector[i].producator) + 1, vector[i].producator);
-			return l;
-		}
-	}
-	l.id = 1;
-
-	return l;
-}
-
-
-
-int main() {
-
-	struct Laptop* laptopuri = NULL;
-	int nrLaptop = 3;
-	laptopuri = (struct Laptop*)malloc(sizeof(struct Laptop) * nrLaptop);
-	laptopuri[0] = initializare(1, 256, "Dell", 2000, 'E');
-	laptopuri[1] = initializare(2, 512, "Lenovo", 3000, 'I');
-	laptopuri[2] = initializare(1, 256, "Asus", 2500, 'G');
-	afisareVector(laptopuri, nrLaptop);
-
-	struct Laptop* primeleLaptopuri = NULL;
-	int nrPrimeleLaptopuri = 2;
-	primeleLaptopuri = copiazaPrimeleNElemente(laptopuri, nrLaptop, nrPrimeleLaptopuri);
-	printf("\nPrimele laptopuri:\n");
-	afisareVector(primeleLaptopuri, nrPrimeleLaptopuri);
-	dezalocare(&primeleLaptopuri, &nrPrimeleLaptopuri);
-	afisareVector(primeleLaptopuri, nrPrimeleLaptopuri);
-
-	//copiaza laptopuri scumpe
-	struct Laptop* laptopuriScumpe = NULL;
-	int nrLaptopuriScumpe = 0;
-	copiazaLaptopuriScumpe(laptopuri, nrLaptop, 2500, &laptopuriScumpe, &nrLaptopuriScumpe);
-	printf("\nLaptopuri scumpe: \n");
-	afisareVector(laptopuriScumpe, nrLaptopuriScumpe);
-	dezalocare(&laptopuriScumpe, &nrLaptopuriScumpe);
-
-	struct Laptop laptop = getPrimulLaptopByProducator(laptopuri, nrLaptop, "Lenovo");
-	printf("\nLaptop by producator \n");
-	afisare(laptop);
-	if (laptop.producator != NULL) {
-		free(laptop.producator);
-		laptop.producator = NULL;
-	}
-	dezalocare(&laptopuri, &nrLaptop);
-	return 0;
-}
+//#include<stdio.h>
+//#include<malloc.h>
+//
+//
+//struct Laptop {
+//	int id;
+//	int RAM;
+//	char* producator;
+//	float pret;
+//	char serie;
+//};
+//
+//struct Laptop initializare(int id, int
+//	ram, const char* producator, float pret, char serie) {
+//	struct Laptop l;
+//	l.id = id;
+//	l.RAM = ram;
+//	l.producator = (char*)malloc(sizeof(char) * (strlen(producator) + 1));
+//	strcpy_s(l.producator, strlen(producator) + 1, producator);
+//	l.pret = pret;
+//	l.serie = serie;
+//	return l;
+//}
+//
+//void afisare(struct Laptop l) {
+//	if (l.producator != NULL) {
+//		printf("%d. Laptopul %s seria %c are %d GB RAM si costa %5.2f RON.\n", l.id, l.producator, l.serie, l.RAM, l.pret);
+//	}
+//	else {
+//		printf("%d. Laptopul din seria %c are %d GB RAM si costa %5.2f RON.\n", l.id, l.serie, l.RAM, l.pret);
+//	}
+//}
+//
+//void afisareVector(struct Laptop* vector, int nrElemente) {
+//	for (int i = 0; i < nrElemente; i++) {
+//		afisare(vector[i]);
+//	}
+//}
+//
+//struct Laptop* copiazaPrimeleNElemente(struct Laptop* vector, int nrElemente, int nrElementeCopiate) {
+//	struct Laptop* vectorNou = NULL;
+//	vectorNou = (struct Laptop*)malloc(sizeof(struct Laptop) * nrElementeCopiate);
+//	for (int i = 0; i < nrElementeCopiate; i++) {
+//		vectorNou[i] = vector[i];
+//		vectorNou[i].producator = (char*)malloc(strlen(vector[i].producator) + 1);
+//		strcpy_s(vectorNou[i].producator, strlen(vector[i].producator) + 1, vector[i].producator);
+//	}
+//
+//	return vectorNou;
+//}
+//
+//void dezalocare(struct Laptop** vector, int* nrElemente) {
+//	for (int i = 0; i < (*nrElemente); i++) {
+//		if ((*vector)[i].producator != NULL){
+//		free((*vector)[i].producator);
+//		}
+//	}
+//	free(*vector);
+//	*vector = NULL;
+//	*nrElemente = 0;
+//}
+//
+//void copiazaLaptopuriScumpe(struct Laptop* vector, char nrElemente, float pretMinim, struct Laptop** vectorNou, int* dimensiune) {
+//	*dimensiune = 0;
+//	for (int i = 0; i < nrElemente; i++) {
+//		if (vector[i].pret >= pretMinim) {
+//			(*dimensiune)++;
+//		}
+//	}
+//	if ((*vectorNou) != NULL){
+//		free(*vectorNou);
+//	}
+//	*vectorNou = (struct Laptop*)malloc(sizeof(struct Laptop) * (*dimensiune));
+//	int k = 0;
+//	for (int i = 0; i < nrElemente; i++) {
+//		if (vector[i].pret >= pretMinim) {
+//			(*vectorNou)[k] = vector[i];
+//			(*vectorNou)[k].producator = (char*)malloc(strlen(vector[i].producator) + 1);
+//			strcpy_s((*vectorNou)[k].producator, strlen(vector[i].producator) + 1, vector[i].producator);
+//			k++;
+//		}
+//	}
+//}
+//
+//struct Laptop getPrimulLaptopByProducator(struct Laptop* vector, int nrElemente, const char* producator) {
+//	struct Laptop l;
+//	for (int i = 0; i < nrElemente; i++) {
+//		if (strcmp(vector[i].producator, producator) == 0) {
+//			l = vector[i];
+//			l.producator = (char*)malloc(strlen(vector[i].producator) + 1);
+//			strcpy_s(l.producator, strlen(vector[i].producator) + 1, vector[i].producator);
+//			return l;
+//		}
+//	}
+//	l.id = 1;
+//
+//	return l;
+//}
+//
+//
+//
+//int main() {
+//
+//	struct Laptop* laptopuri = NULL;
+//	int nrLaptop = 3;
+//	laptopuri = (struct Laptop*)malloc(sizeof(struct Laptop) * nrLaptop);
+//	laptopuri[0] = initializare(1, 256, "Dell", 2000, 'E');
+//	laptopuri[1] = initializare(2, 512, "Lenovo", 3000, 'I');
+//	laptopuri[2] = initializare(1, 256, "Asus", 2500, 'G');
+//	afisareVector(laptopuri, nrLaptop);
+//
+//	struct Laptop* primeleLaptopuri = NULL;
+//	int nrPrimeleLaptopuri = 2;
+//	primeleLaptopuri = copiazaPrimeleNElemente(laptopuri, nrLaptop, nrPrimeleLaptopuri);
+//	printf("\nPrimele laptopuri:\n");
+//	afisareVector(primeleLaptopuri, nrPrimeleLaptopuri);
+//	dezalocare(&primeleLaptopuri, &nrPrimeleLaptopuri);
+//	afisareVector(primeleLaptopuri, nrPrimeleLaptopuri);
+//
+//	//copiaza laptopuri scumpe
+//	struct Laptop* laptopuriScumpe = NULL;
+//	int nrLaptopuriScumpe = 0;
+//	copiazaLaptopuriScumpe(laptopuri, nrLaptop, 2500, &laptopuriScumpe, &nrLaptopuriScumpe);
+//	printf("\nLaptopuri scumpe: \n");
+//	afisareVector(laptopuriScumpe, nrLaptopuriScumpe);
+//	dezalocare(&laptopuriScumpe, &nrLaptopuriScumpe);
+//
+//	struct Laptop laptop = getPrimulLaptopByProducator(laptopuri, nrLaptop, "Lenovo");
+//	printf("\nLaptop by producator \n");
+//	afisare(laptop);
+//	if (laptop.producator != NULL) {
+//		free(laptop.producator);
+//		laptop.producator = NULL;
+//	}
+//	dezalocare(&laptopuri, &nrLaptop);
+//	return 0;
+//}
--- Laborator03.c
+++ Laborator03.c
@@ -1,94 +1,94 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-struct StructuraLaptop {
-	int id;
-	int nrNuclee;
-	float pret;
-	char* model;
-	char* producator;
-	unsigned char serie;
-};
-typedef struct StructuraLaptop Laptop;
-
-void afisareLaptop(Laptop laptop) {
-	printf("ID:%d\n", laptop.id);
-	printf("Numar nuclee: %d\n", laptop.nrNuclee);
-	printf("Pretul: %.2f \n", laptop.pret);
-	printf("Model: %s\n", laptop.model);
-	printf("Producator: %s\n", laptop.producator);
-	printf("Serie: %c\n", laptop.serie);
-
-}
-
-void afisareVectorLaptop(Laptop* laptopuri, int nrLaptopuri) {
-	for (int i = 0; i < nrLaptopuri; i++) {
-		afisareLaptop(laptopuri[i]);
-	}
-}
-
-void adaugaLaptopInVector(Laptop** laptopuri, int* nrLaptopuri, Laptop laptopNou) {
-	Laptop* aux = (Laptop*)malloc(sizeof(Laptop) * ((*nrLaptopuri) + 1));
-	for (int i = 0; i < *nrLaptopuri; i++) {
-		aux[i] = (*laptopuri)[i];
-	}
-	aux[(*nrLaptopuri)] = laptopNou;
-	free(*laptopuri);
-	(*laptopuri) = aux;
-	(*nrLaptopuri)++;
-}
-
-Laptop citireLaptopFisier(FILE* file) {
-	Laptop l;
-	char buffer[100];
-	char sep[4] = ",;\n";
-	fgets(buffer, 100, file);
-	l.id = atoi(strtok(buffer, sep));
-	l.nrNuclee = atoi(strtok(NULL, sep));
-	l.pret = atof(strtok(NULL, sep));
-	char* aux;
-	aux = strtok(NULL, sep);
-	l.model = (char*)malloc(strlen(aux) + 1);
-	strcpy(l.model, aux);
-	aux = strtok(NULL, sep);
-	l.producator = (char*)malloc(strlen(aux) + 1);
-	strcpy(l.producator, aux);
-	l.serie = strtok(NULL, sep)[0];
-	return l;
-}
-
-Laptop* citireVectorLaptopFisier(const char* numeFisier, int* nrLaptopCitite) {
-	FILE* file = fopen(numeFisier, "r");
-	Laptop* laptop = NULL;
-	(*nrLaptopCitite) = 0;
-	while (!feof(file)) {
-		adaugaLaptopInVector(&laptop, nrLaptopCitite, citireLaptopFisier(file));
-	}
-	fclose(file);
-	return laptop;
-}
-
-void dezalocareVectorLaptop(Laptop** vector, int* nrLaptopuri) {
-	for (int i = 0; i < *nrLaptopuri; i++) {
-		if ((*vector)[i].model != NULL) {
-			free((*vector)[i].model);
-		}
-		if ((*vector)[i].producator != NULL) {
-			free((*vector)[i].producator);
-		}
-	}
-	free(*vector);
-	(*vector) = NULL;
-	(*nrLaptopuri) = 0;
-	
-}
-
-int main() {
-	int nrLaptopuri = 0;
-	Laptop* laptopuri = citireVectorLaptopFisier("laptopuri.txt", &nrLaptopuri);
-	afisareVectorLaptop(laptopuri, nrLaptopuri);
-
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+//struct StructuraLaptop {
+//	int id;
+//	int nrNuclee;
+//	float pret;
+//	char* model;
+//	char* producator;
+//	unsigned char serie;
+//};
+//typedef struct StructuraLaptop Laptop;
+//
+//void afisareLaptop(Laptop laptop) {
+//	printf("ID:%d\n", laptop.id);
+//	printf("Numar nuclee: %d\n", laptop.nrNuclee);
+//	printf("Pretul: %.2f \n", laptop.pret);
+//	printf("Model: %s\n", laptop.model);
+//	printf("Producator: %s\n", laptop.producator);
+//	printf("Serie: %c\n", laptop.serie);
+//
+//}
+//
+//void afisareVectorLaptop(Laptop* laptopuri, int nrLaptopuri) {
+//	for (int i = 0; i < nrLaptopuri; i++) {
+//		afisareLaptop(laptopuri[i]);
+//	}
+//}
+//
+//void adaugaLaptopInVector(Laptop** laptopuri, int* nrLaptopuri, Laptop laptopNou) {
+//	Laptop* aux = (Laptop*)malloc(sizeof(Laptop) * ((*nrLaptopuri) + 1));
+//	for (int i = 0; i < *nrLaptopuri; i++) {
+//		aux[i] = (*laptopuri)[i];
+//	}
+//	aux[(*nrLaptopuri)] = laptopNou;
+//	free(*laptopuri);
+//	(*laptopuri) = aux;
+//	(*nrLaptopuri)++;
+//}
+//
+//Laptop citireLaptopFisier(FILE* file) {
+//	Laptop l;
+//	char buffer[100];
+//	char sep[4] = ",;\n";
+//	fgets(buffer, 100, file);
+//	l.id = atoi(strtok(buffer, sep));
+//	l.nrNuclee = atoi(strtok(NULL, sep));
+//	l.pret = atof(strtok(NULL, sep));
+//	char* aux;
+//	aux = strtok(NULL, sep);
+//	l.model = (char*)malloc(strlen(aux) + 1);
+//	strcpy(l.model, aux);
+//	aux = strtok(NULL, sep);
+//	l.producator = (char*)malloc(strlen(aux) + 1);
+//	strcpy(l.producator, aux);
+//	l.serie = strtok(NULL, sep)[0];
+//	return l;
+//}
+//
+//Laptop* citireVectorLaptopFisier(const char* numeFisier, int* nrLaptopCitite) {
+//	FILE* file = fopen(numeFisier, "r");
+//	Laptop* laptop = NULL;
+//	(*nrLaptopCitite) = 0;
+//	while (!feof(file)) {
+//		adaugaLaptopInVector(&laptop, nrLaptopCitite, citireLaptopFisier(file));
+//	}
+//	fclose(file);
+//	return laptop;
+//}
+//
+//void dezalocareVectorLaptop(Laptop** vector, int* nrLaptopuri) {
+//	for (int i = 0; i < *nrLaptopuri; i++) {
+//		if ((*vector)[i].model != NULL) {
+//			free((*vector)[i].model);
+//		}
+//		if ((*vector)[i].producator != NULL) {
+//			free((*vector)[i].producator);
+//		}
+//	}
+//	free(*vector);
+//	(*vector) = NULL;
+//	(*nrLaptopuri) = 0;
+//	
+//}
+//
+//int main() {
+//	int nrLaptopuri = 0;
+//	Laptop* laptopuri = citireVectorLaptopFisier("laptopuri.txt", &nrLaptopuri);
+//	afisareVectorLaptop(laptopuri, nrLaptopuri);
+//
+//	return 0;
+//}
